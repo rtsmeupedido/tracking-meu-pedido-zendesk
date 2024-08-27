c9908fd6fb48d34ef40760c9289fef1b0def50ee
rtsmeupedido <jpbotelho@runtask.com>
2024-08-27 10:43:07 -0300

    fix: removido arquivos desnecessarios

--- meupedido/src/components/Render/store/index.tsx
+++ meupedido/src/components/Render/store/index.tsx
@@ -1,36 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import userReducer from "../features/user/User";
-import softPhoneReducer from "../features/softphone/Call";
 import zendeskReducer from "../features/zendesk";
-import sessionPhoneReducer from "../features/softphone/Session";
-import phoneSlice from "../features/phoneSlice/phoneSlice";
 
 export const store = configureStore({
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Ignore these action types
-        ignoredActions: [
-          "softphoneSlice/addCall",
-          "softphoneSlice/setCallMuted",
-          "softphoneSlice/setCallHolded",
-          "sessionSlice/setUserSession",
-        ],
-        // Ignore these field paths in all actions
-        ignoredActionPaths: ["session.data", "softphone.data"],
-        // Ignore these paths in the state
-        ignoredPaths: ["session.data", "softphone.data"],
-      },
-    }),
-  reducer: {
-    user: userReducer,
-    softphone: softPhoneReducer,
-    counter: counterReducer,
-    session: sessionPhoneReducer,
-    zendesk: zendeskReducer,
-    phone: phoneSlice,
-  },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {},
+        }),
+    reducer: {
+        user: userReducer,
+        zendesk: zendeskReducer,
+    },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
